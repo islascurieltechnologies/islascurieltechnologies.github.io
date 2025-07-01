e3f9ac57c9cfa441bf4e9c710af40adc1670f395
islascurieltechnologies <lalo@islascurieltechnologies.com>
2025-06-30 22:51:26 -0600

    Update sonido.js
    Paso número 2 estructura y modificada para acceso a la segunda parte del sitio con reproducción de audio

--- sonido.js
+++ sonido.js
@@ -1,4 +1,14 @@
-document.getElementById("entrar").addEventListener("click", () => {
+document.addEventListener("DOMContentLoaded", function () {
+  const boton = document.getElementById("entrar");
   const audio = new Audio("entrada.mp3");
-  audio.play();
+
+  boton.addEventListener("click", function () {
+    audio.play();
+
+    // Esperamos 1 segundo (1000 ms) para mostrar el contenido
+    setTimeout(() => {
+      document.getElementById("bienvenida").style.display = "none";
+      document.getElementById("contenido").style.display = "block";
+    }, 1000);
+  });
 });
